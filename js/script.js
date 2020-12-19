2c1fc7062cd6f305621c648a833cee16d1903ef6
Piotr Pepliński <p.peplaj@gmail.com>
2020-12-19 13:23:23 +0100

    added fade in and attention seeking animations

--- js/script.js
+++ js/script.js
@@ -1,31 +1,64 @@
-/* ------------------------------- */
-/* READ MORE BOXES */
-/* ------------------------------- */
+$(document).ready(function () {
 
-/* OPENING */
-document.getElementById("popupLink1").onclick = function () {
-  document.getElementById("popup1").style.display = "flex";
-};
-document.getElementById("popupLink2").onclick = function () {
-  document.getElementById("popup2").style.display = "flex";
-};
-document.getElementById("popupLink3").onclick = function () {
-  document.getElementById("popup3").style.display = "flex";
-};
-document.getElementById("popupLink4").onclick = function () {
-  document.getElementById("popup4").style.display = "flex";
-};
+  /* ------------------------------- */
+  /* READ MORE BOXES */
+  /* ------------------------------- */
 
-/* CLOSING */
-document.getElementById("close1").onclick = function () {
-  document.getElementById("popup1").style.display = "none";
-};
-document.getElementById("close2").onclick = function () {
-  document.getElementById("popup2").style.display = "none";
-};
-document.getElementById("close3").onclick = function () {
-  document.getElementById("popup3").style.display = "none";
-};
-document.getElementById("close4").onclick = function () {
-  document.getElementById("popup4").style.display = "none";
-};
+  /* OPENING */
+  document.getElementById("popupLink1").onclick = function () {
+    document.getElementById("popup1").style.display = "flex";
+  };
+  document.getElementById("popupLink2").onclick = function () {
+    document.getElementById("popup2").style.display = "flex";
+  };
+  document.getElementById("popupLink3").onclick = function () {
+    document.getElementById("popup3").style.display = "flex";
+  };
+  document.getElementById("popupLink4").onclick = function () {
+    document.getElementById("popup4").style.display = "flex";
+  };
+
+  /* CLOSING */
+  document.getElementById("close1").onclick = function () {
+    document.getElementById("popup1").style.display = "none";
+  };
+  document.getElementById("close2").onclick = function () {
+    document.getElementById("popup2").style.display = "none";
+  };
+  document.getElementById("close3").onclick = function () {
+    document.getElementById("popup3").style.display = "none";
+  };
+  document.getElementById("close4").onclick = function () {
+    document.getElementById("popup4").style.display = "none";
+  };
+
+  /* ------------------------------- */
+  /* ANIMATIONS */
+  /* ------------------------------- */
+  $('.section-me').waypoint(function (direction) {
+    $('.js-insta-me').addClass('animate__animated animate__fadeInLeftBig');
+  }, { offset: '10%' });
+
+/* free section */
+  $('.section-free').waypoint(function (direction) {
+    $('.js-redu-cover').addClass('animate__animated animate__fadeInUpBig');
+  }, { offset: '10%' });
+
+  $('.section-free').waypoint(function (direction) {
+    $('.js-free-email').addClass('animate__animated animate__heartBeat animate__delay-2s');
+  }, { offset: '-10%' });
+
+/* Ebooks fade in */
+  $('.section-ebooks').waypoint(function (direction) {
+    $('.js-ebook-1').addClass('animate__animated animate__fadeInUpBig');
+  }, { offset: '-10%' });
+
+  $('.section-ebooks').waypoint(function (direction) {
+    $('.js-ebook-2').addClass('animate__animated animate__fadeInUpBig animate__delay-1s');
+  }, { offset: '-10%' });
+
+  $('.section-ebooks').waypoint(function (direction) {
+    $('.js-ebook-3').addClass('animate__animated animate__fadeInUpBig animate__delay-2s');
+  }, { offset: '-10%' });
+
+});	
